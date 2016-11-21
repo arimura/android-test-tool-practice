24046987d7dc13b852bc6bcce886781e80e273b6
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2016-11-21 15:08:53 +0900

    add activity under test

---
+++ app/src/main/java/com/hormiga6/androidtesttoolpractice/ListActivity.java
@@ -0,0 +1,35 @@
+package com.hormiga6.androidtesttoolpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        adapter.addAll(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"));
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog alertDialog = new AlertDialog.Builder(ListActivity.this).create();
+                alertDialog.setMessage(String.format("This is %s", parent.getAdapter().getItem(position).toString()));
+                alertDialog.show();
+            }
+        });
+    }
+}
