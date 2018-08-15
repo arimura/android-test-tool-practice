1b79ce8a567b5cd1024e9ced6182ae72c1a8285d
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-08-15 19:22:19 +0900

    update build setting

--- app/src/test/java/com/hormiga6/androidtesttoolpractice/MockitoTest.java
+++ app/src/test/java/com/hormiga6/androidtesttoolpractice/MockitoTest.java
@@ -8,6 +8,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
